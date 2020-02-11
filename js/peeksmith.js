c5c59f964c25488d8a8093ec7a8a53e19a17e561
Bártházi András <boogie@BoogiePro3000.local>
2020-02-11 06:12:15 +0100

    wrapping text, handling disconnect

--- js/peeksmith.js
+++ js/peeksmith.js
@@ -6,6 +6,7 @@ let characteristic;
 
 const connectButton = document.getElementById('button-connect');
 const messageTextField = document.getElementById('textarea-message');
+const checkboxWrapText = document.getElementById('checkbox-wrap-text');
 
 connectButton.addEventListener('click', connectDevice);
 messageTextField.addEventListener('keyup', messageKeyUp);
@@ -21,14 +22,21 @@ async function requestDevice() {
 
 async function connectDevice() {
     const device = await requestDevice();
+    device.addEventListener('gattserverdisconnected', deviceDisconnected);
     const server = await device.gatt.connect();
     const service = await server.getPrimaryService(SERVICE_UUID);
     characteristic = await service.getCharacteristic(CHARACTERISTIC_UUID);
     await characteristic.writeValue(encoder.encode("/T5\n"));
+    messageTextField.value = '';
     document.getElementById('initial-screen').style.display = 'none';
     document.getElementById('when-connected').style.display = 'block';
     messageTextField.focus();
     setInterval(_display, 100);
+}
+
+function deviceDisconnected() {
+    document.getElementById('initial-screen').style.display = 'block';
+    document.getElementById('when-connected').style.display = 'none';
 }
 
 let queue = [];
@@ -36,7 +44,47 @@ let inProgress = false;
 
 function messageKeyUp() {
     const message = this.value;
-    display(`#${message.substring(0, 84)}\n`);
+    const wrap = checkboxWrapText.checked;
+    const formattedMessage = formatMessage(message, wrap);    
+    display(formattedMessage);
+}
+
+function formatMessage(message, wrap) {
+    let formattedMessage = wrap ? wrapMessage(message) : message;
+    return `#${formattedMessage}\n`;
+}
+
+function wrapMessage(message, step = 0) {
+    const steps = [
+        { columns: 7, rows: 1 },
+        { columns: 10, rows: 2 },
+        { columns: 21, rows: 4 }
+    ];
+    const constraints = steps[step];
+    let words = message.split(/\s+/);
+    let rows = [''];
+    words.forEach(word => {
+        if (rows[0].length === constraints.columns) {
+            rows.unshift('');
+        }
+        let needsSpace = rows[0].length > 0;
+        if (rows[0].length + (needsSpace ? 1 : 0) + word.length <= constraints.columns) {
+            if (needsSpace) rows[0] += ' ';
+            rows[0] += word;
+        } else {
+            if (word.length < constraints.columns) {
+                rows.unshift(word);
+            } else {
+                while (word.length) {
+                    rows.unshift(word.substring(0, constraints.columns));
+                    word = word.substring(constraints.columns);
+                }
+            }
+        }
+    });
+    rows = rows.map(row => row + ' '.repeat(constraints.columns - row.length));
+    if (rows.length > constraints.rows && step < 2) return wrapMessage(message, step + 1);
+    return rows.reverse().join('');
 }
 
 function display(message) {
@@ -47,11 +95,9 @@ function display(message) {
 }
 
 function _display() {
-    if (inProgress) return;
+    if (inProgress || queue.length === 0) return;
     let buffer = queue.pop();
-    if (typeof buffer === 'undefined') return;
     inProgress = true;
-    console.log(buffer);
     const maxLength = 19;
     function send() {
         if (buffer.length === 0) {
